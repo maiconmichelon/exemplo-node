94ff72731701642ae6d8d92b675df0f885539d5d
maiconmichelon <maicon.fermo@gmail.com>
2014-11-16 21:50:28 -0200

    session compartilhada

--- ntalk/ntalk/sockets/chat.js
+++ ntalk/ntalk/sockets/chat.js
@@ -2,9 +2,14 @@ module.exports = function(io) {
   var sockets = io.sockets;
   sockets.on('connection', function (client) {
     client.on('send-server', function (data) {
-      var msg = "<b>"+data.nome+":</b> "+data.msg+"<br>";
-      client.emit('send-client', msg);
-      client.broadcast.emit('send-client', msg);
+      var session = client.handshake.session
+          , usuario = session.usuario;
+
+      client.on('send-server', function (msg) {
+        msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+        client.emit('send-client', msg);
+        client.broadcast.emit('send-client', msg);
+      });
     });
   });
 }
