7b86c043892c53e661c9dde5a5f88eec720fcbd6
maiconmichelon <maicon.fermo@gmail.com>
2014-11-16 23:55:52 -0200

    Terminando cap 6

--- ntalk/ntalk/sockets/chat.js
+++ ntalk/ntalk/sockets/chat.js
@@ -2,8 +2,20 @@ module.exports = function(io) {
   var crypto = require('crypto')
     , md5 = crypto.createHash('md5')
     , sockets = io.sockets;
-  
+
   sockets.on('connection', function (client) {
+    var session = client.handshake.session
+        , usuario = session.usuario;
+
+    var onlines = sockets.clients();
+    onlines.forEach(function(online) {
+      var online = sockets.sockets[online.id];
+      online.get('email', function(err, email) {
+        client.emit('notify-onlines', email);
+        client.broadcast.emit('notify-onlines', email);
+      });
+    });
+
     client.on('join', function(sala) {
       if(sala) {
         sala = sala.replace('?','');
@@ -22,17 +34,21 @@ module.exports = function(io) {
       });
     });
 
-    client.on('send-server', function (data) {
-      var session = client.handshake.session
-          , usuario = session.usuario;
+    client.on('send-server', function (msg) {
+      msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
+      client.get('sala', function(erro, sala) {
+        var data = {email: usuario.email, sala: sala};
+        client.broadcast.emit('new-message', data);
+        sockets.in(sala).emit('send-client', msg);
+      });
+    });
 
-      client.on('send-server', function (msg) {
-        msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
-        client.get('sala', function(erro, sala) {
-          var data = {email: usuario.email, sala: sala};
-          client.broadcast.emit('new-message', data);
-          sockets.in(sala).emit('send-client', msg);
-        });
+    client.on('disconnect', function() {
+      client.get('sala', function(erro, sala) {
+        var msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+        client.broadcast.emit('notify-offline', usuario.email);
+        sockets.in(sala).emit('send-client', msg);
+        client.leave(sala);
       });
     });
   });
